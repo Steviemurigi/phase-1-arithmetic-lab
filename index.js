dd3e7e5445c7a11af216c2e0740207a647b6ab03
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-07-05 09:22:51 -0500

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let multiply = (num1 * num2)
+let num1 = 31
+let num2 = 2
+multiply= 62
+let random = Math.floor(Math.random()* 100)+1;
+let mod = (num3 % num4)
+let num3= 28
+let num4= 6
+mod = (28 % 6)
+let max = Math.max (15,16,17,18,19,20)
 
-// Write your code here
